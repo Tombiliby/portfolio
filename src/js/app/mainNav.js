d957793fcdfe6b845ceb8c4540901b91968741cc
Thomas Champion <thomas.champion@orchestra.eu>
2017-09-26 17:32:50 +0200

    optim style

--- src/js/app/mainNav.js
+++ src/js/app/mainNav.js
@@ -1,7 +1,6 @@
 $(".btn_mainMenu_toggle").on("click", function(){
 	var bodyPosScroll = $(window).scrollTop();
 
-	console.log(bodyPosScroll);
 	if(!$("body").hasClass("main_nav_open")){
 		$("body").toggleClass("main_nav_open");
 	} else {
